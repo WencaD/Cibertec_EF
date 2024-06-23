4471733ef87bec6dde3392915349ca36a65c7361
WencaD <wencad18@gmail.com>
2024-06-23 01:28:18 -0500

    Inicio del proyecto

---
+++ src/cibertec_ef/Cibertec_EF.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cibertec_ef;
+
+/**
+ *
+ * @author omarc
+ */
+public class Cibertec_EF {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
